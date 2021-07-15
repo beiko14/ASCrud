197d43aa07de42dbeeb850e0b13227dfad379ba7
Beiko14 <heiko.hollerith14@gmx.de>
2021-07-15 11:38:43 +0200

    use HttpClientModule to call the REST endpoint added @Crossoriging to the Backend to enable the access to XMLHttpRequest

--- angular-frontend/src/app/employee-list/employee-list.component.ts
+++ angular-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,23 +11,16 @@ export class EmployeeListComponent implements OnInit {
 
   employees!: Employee[];
 
-  constructor() { }
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.employees = [
-      {
-        "id": 1,
-        "firstName": "Julia",
-        "lastName": "Schmidt",
-        "emailId": "juliaschmidt@test.de"
-      },
-      {
-        "id": 2,
-        "firstName": "Hans",
-        "lastName": "Baum",
-        "emailId": "hansbaum@test.de"
-      }
-    ];
+    this.getEmployees();
+  }
+
+  private getEmployees(){
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
   }
 
 }
