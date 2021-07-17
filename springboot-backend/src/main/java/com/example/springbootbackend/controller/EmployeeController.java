e2b5011e74a74564e56ea31037a69e397ee0b36b
Beiko14 <heiko.hollerith14@gmx.de>
2021-07-17 10:37:33 +0200

    created getEmployeeById REST API

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.example.springbootbackend.controller;
 
+import com.example.springbootbackend.exception.ResourceNotFoundException;
 import com.example.springbootbackend.model.Employee;
 import com.example.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +25,13 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("There is no employee with the following id: " + id));
+        return ResponseEntity.ok(employee);
     }
 }
 
