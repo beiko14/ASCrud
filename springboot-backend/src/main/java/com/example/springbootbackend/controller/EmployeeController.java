af47788e3cab242a9a424a6ccf4db4cc7eed1f3e
Beiko14 <heiko.hollerith14@gmx.de>
2021-07-18 11:46:28 +0200

    specified the @CrossOrigin annotation

--- springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
